edc50db0f5914e03619135133a4143666804dba4
TANISH6727 <22b0978@iitb.ac.in>
2024-04-14 15:22:14 +0530

    Added ocr.py

--- ocr.py
+++ ocr.py
@@ -1,12 +1,13 @@
 import pytesseract
 import cv2 as cv
 import string
-chars= string.ascii_letters + string.digits
+chars= string.digits
 count = 0 
 for c in chars:
-    img = cv.imread(f"character_{c}.png")
-    ocr_result= pytesseract.image_to_string(img,config= '--psm 6')
-    if(ocr_result.strip()==c):
+    img = cv.imread(f"chars/character_{c}.png")
+    ocr_result= pytesseract.image_to_string(img,config='--psm 10')
+    if(ocr_result.strip()!=c):
+        print(c," Extracted text: ",ocr_result)
+    else:
         count+=1
-    print("Extracted text: ",ocr_result)
-print("Accuracy",count/62)
+print("Accuracy",count/26)
